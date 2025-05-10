9972be6ec75050928093cd0a0fb303888cbd2379
fetters <997167305@qq.com>
2025-05-10 14:54:31 +0800

    集成 mybatis-plus 集成 hultool 工具包 集成 knife4j 接口文档 集成 Aop

--- src/main/java/com/fetters/picture/PictureApplication.java
+++ src/main/java/com/fetters/picture/PictureApplication.java
@@ -1,9 +1,13 @@
 package com.fetters.picture;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.fetters.picture.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class PictureApplication {
 
     public static void main(String[] args) {

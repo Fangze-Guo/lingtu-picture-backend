d2009496ef4904be94e711d27cdb53204d3c84eb
fetters <997167305@qq.com>
2025-05-30 13:27:11 +0800

    图片模块初始代码

---
+++ src/main/java/com/fetters/picture/mapper/PictureMapper.java
@@ -0,0 +1,18 @@
+package com.fetters.picture.mapper;
+
+import com.fetters.picture.model.entity.Picture;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 99716
+* @description 针对表【picture(图片)】的数据库操作Mapper
+* @createDate 2025-05-29 20:12:46
+* @Entity com.fetters.picture.model.entity.Picture
+*/
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
+
+
+
+

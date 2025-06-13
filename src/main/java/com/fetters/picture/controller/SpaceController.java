1dbdddd1247d20604b2b807059cb821bd5494c6e
fetters <997167305@qq.com>
2025-06-13 16:01:43 +0800

    add 空间管理接口

---
+++ src/main/java/com/fetters/picture/controller/SpaceController.java
@@ -0,0 +1,196 @@
+package com.fetters.picture.controller;
+
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fetters.picture.annotation.AuthCheck;
+import com.fetters.picture.common.BaseResponse;
+import com.fetters.picture.common.DeleteRequest;
+import com.fetters.picture.common.ResultUtils;
+import com.fetters.picture.constant.UserConstant;
+import com.fetters.picture.exception.BusinessException;
+import com.fetters.picture.exception.ErrorCode;
+import com.fetters.picture.exception.ThrowUtils;
+import com.fetters.picture.model.dto.space.SpaceEditRequest;
+import com.fetters.picture.model.dto.space.SpaceQueryRequest;
+import com.fetters.picture.model.dto.space.SpaceUpdateRequest;
+import com.fetters.picture.model.entity.Space;
+import com.fetters.picture.model.entity.User;
+import com.fetters.picture.model.vo.SpaceVO;
+import com.fetters.picture.service.SpaceService;
+import com.fetters.picture.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.redis.core.Cursor;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author : Fetters
+ * @description : 空间控制层
+ * @createDate : 2025/5/30 10:16
+ */
+@Slf4j
+@RestController
+@RequestMapping("/space")
+public class SpaceController {
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private SpaceService spaceService;
+
+    /**
+     * 删除空间
+     */
+    @PostMapping("/delete")
+    public BaseResponse<Boolean> deleteSpace(@RequestBody DeleteRequest deleteRequest, HttpServletRequest request) {
+        if (deleteRequest == null || deleteRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        long id = deleteRequest.getId();
+        // 判断是否存在
+        Space oldSpace = spaceService.getById(id);
+        ThrowUtils.throwIf(oldSpace == null, ErrorCode.NOT_FOUND_ERROR);
+        // 仅本人或管理员可删除
+        if (!oldSpace.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        // 操作数据库
+        boolean result = spaceService.removeById(id);
+        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
+        return ResultUtils.success(true);
+    }
+
+    /**
+     * 更新空间（仅管理员可用）
+     */
+    @PostMapping("/update")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Boolean> updateSpace(@RequestBody SpaceUpdateRequest spaceUpdateRequest,
+                                             HttpServletRequest request) {
+        if (spaceUpdateRequest == null || spaceUpdateRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 判断是否存在
+        long id = spaceUpdateRequest.getId();
+        Space oldSpace = spaceService.getById(id);
+        ThrowUtils.throwIf(oldSpace == null, ErrorCode.NOT_FOUND_ERROR);
+
+        // 将实体类和 DTO 进行转换
+        Space space = new Space();
+        BeanUtils.copyProperties(spaceUpdateRequest, space);
+        // 自动填充数据
+        spaceService.fillSpaceBySpaceLevel(space);
+        // 数据校验
+        spaceService.validSpace(space, false);
+
+        // 操作数据库
+        boolean result = spaceService.updateById(space);
+        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
+        return ResultUtils.success(true);
+    }
+
+    /**
+     * 编辑空间（用户使用）
+     */
+    @PostMapping("/edit")
+    public BaseResponse<Boolean> editSpace(@RequestBody SpaceEditRequest spaceEditRequest, HttpServletRequest request) {
+        if (spaceEditRequest == null || spaceEditRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 判断是否存在空间
+        long id = spaceEditRequest.getId();
+        Space oldSpace = spaceService.getById(id);
+        ThrowUtils.throwIf(oldSpace == null, ErrorCode.NOT_FOUND_ERROR);
+        // 仅本人或管理员可编辑
+        User loginUser = userService.getLoginUser(request);
+        if (!oldSpace.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+
+        // 在此处将实体类和 DTO 进行转换
+        Space space = new Space();
+        BeanUtils.copyProperties(spaceEditRequest, space);
+        // 自动填充数据
+        spaceService.fillSpaceBySpaceLevel(space);
+        // 设置编辑时间
+        space.setEditTime(new Date());
+        // 数据校验
+        spaceService.validSpace(space, false);
+
+        // 操作数据库
+        boolean result = spaceService.updateById(space);
+        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
+        return ResultUtils.success(true);
+    }
+
+    /**
+     * 根据 id 获取空间（仅管理员可用）
+     */
+    @GetMapping("/get")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Space> getSpaceById(long id, HttpServletRequest request) {
+        ThrowUtils.throwIf(id <= 0, ErrorCode.PARAMS_ERROR);
+        // 查询数据库
+        Space space = spaceService.getById(id);
+        ThrowUtils.throwIf(space == null, ErrorCode.NOT_FOUND_ERROR);
+        // 获取封装类
+        return ResultUtils.success(space);
+    }
+
+    /**
+     * 根据 id 获取空间（用户使用）
+     */
+    @GetMapping("/get/vo")
+    public BaseResponse<SpaceVO> getSpaceVOById(long id, HttpServletRequest request) {
+        ThrowUtils.throwIf(id <= 0, ErrorCode.PARAMS_ERROR);
+        // 查询数据库
+        Space space = spaceService.getById(id);
+        ThrowUtils.throwIf(space == null, ErrorCode.NOT_FOUND_ERROR);
+        // 获取封装类
+        return ResultUtils.success(spaceService.getSpaceVO(space, request));
+    }
+
+    /**
+     * 分页获取空间列表（仅管理员可用）
+     */
+    @PostMapping("/list/page")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Page<Space>> listSpaceByPage(@RequestBody SpaceQueryRequest spaceQueryRequest) {
+        long current = spaceQueryRequest.getCurrent();
+        long size = spaceQueryRequest.getPageSize();
+        // 查询数据库
+        Page<Space> spacePage = spaceService.page(new Page<>(current, size),
+                spaceService.getQueryWrapper(spaceQueryRequest));
+        return ResultUtils.success(spacePage);
+    }
+
+    /**
+     * 分页获取空间列表（用户使用）
+     */
+    @PostMapping("/list/page/vo")
+    public BaseResponse<Page<SpaceVO>> listSpaceVOByPage(@RequestBody SpaceQueryRequest spaceQueryRequest,
+                                                         HttpServletRequest request) {
+        long current = spaceQueryRequest.getCurrent();
+        long size = spaceQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+
+        // 查询数据库
+        Page<Space> spacePage = spaceService.page(new Page<>(current, size),
+                spaceService.getQueryWrapper(spaceQueryRequest));
+        // 获取封装类
+        Page<SpaceVO> spaceVOPage = spaceService.getSpaceVOPage(spacePage, request);
+        return ResultUtils.success(spaceVOPage);
+    }
+}

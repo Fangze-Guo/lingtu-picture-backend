303b91232d52a34dada2af4bcb35fde765e374c7
fetters <997167305@qq.com>
2026-03-14 13:28:30 +0800

    feat: 完善 Sa-Token 空间权限认证功能
      - 新增 HttpRequestWrapperFilter 过滤器，支持请求体重复读取
      - 新增 RequestWrapper 包装类，解决 InputStream 只能读取一次的问题
      - 新增 SpaceUserAuthContext 上下文类，存储空间权限认证信息
      - 新增 StpInterfaceImpl 实现类，实现 Sa-Token 权限接口
      - 完善 SpaceUserAuthManager，新增从请求中获取认证上下文的方法
      - 支持根据空间类型（私有/团队）动态计算用户权限
      - 支持公共图库、私有空间、团队空间的权限控制

--- src/main/java/com/fetters/picture/manager/auth/SpaceUserAuthManager.java
+++ src/main/java/com/fetters/picture/manager/auth/SpaceUserAuthManager.java
@@ -1,17 +1,27 @@
 package com.fetters.picture.manager.auth;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.util.ObjUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.extra.servlet.ServletUtil;
+import cn.hutool.http.ContentType;
+import cn.hutool.http.Header;
 import cn.hutool.json.JSONUtil;
 import com.fetters.picture.manager.auth.model.SpaceUserAuthConfig;
 import com.fetters.picture.manager.auth.model.SpaceUserRole;
 import com.fetters.picture.service.SpaceUserService;
 import com.fetters.picture.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class SpaceUserAuthManager {
@@ -21,6 +31,9 @@ public class SpaceUserAuthManager {
 
     @Resource
     private UserService userService;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
 
     public static final SpaceUserAuthConfig SPACE_USER_AUTH_CONFIG;
 
@@ -45,5 +58,42 @@ public class SpaceUserAuthManager {
             return new ArrayList<>();
         }
         return role.getPermissions();
+    }
+
+    /**
+     * 从请求中获取上下文对象
+     */
+    public SpaceUserAuthContext getAuthContextByRequest() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String contentType = request.getHeader(Header.CONTENT_TYPE.getValue());
+        SpaceUserAuthContext authRequest;
+        // 兼容 get 和 post 操作
+        if (ContentType.JSON.getValue().equals(contentType)) {
+            String body = ServletUtil.getBody(request);
+            authRequest = JSONUtil.toBean(body, SpaceUserAuthContext.class);
+        } else {
+            Map<String, String> paramMap = ServletUtil.getParamMap(request);
+            authRequest = BeanUtil.toBean(paramMap, SpaceUserAuthContext.class);
+        }
+        // 根据请求路径区分 id 字段的含义
+        Long id = authRequest.getId();
+        if (ObjUtil.isNotNull(id)) {
+            String requestUri = request.getRequestURI();
+            String partUri = requestUri.replace(contextPath + "/", "");
+            String moduleName = StrUtil.subBefore(partUri, "/", false);
+            switch (moduleName) {
+                case "picture":
+                    authRequest.setPictureId(id);
+                    break;
+                case "spaceUser":
+                    authRequest.setSpaceUserId(id);
+                    break;
+                case "space":
+                    authRequest.setSpaceId(id);
+                    break;
+                default:
+            }
+        }
+        return authRequest;
     }
 }
